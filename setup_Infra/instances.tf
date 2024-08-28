a28d1c2cc175e86792cddb13ba9bb954af03dade
vishalk17 <vishalkapadi95@gmail.com>
2024-08-28 23:17:31 +0530

    create 2 instances in diff az

--- setup_Infra/instances.tf
+++ setup_Infra/instances.tf
@@ -1,3 +1,5 @@
+# Fetch ami id of latest ubuntu image using datasource
+# Ref. : https://developer.hashicorp.com/terraform/language/data-sources
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -25,4 +27,32 @@ data "aws_ami" "ubuntu" {
 
 output "ami" {
   value = data.aws_ami.ubuntu
+}
+
+# Create instance in az1
+# Ref.: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "pub_instance1" {
+  depends_on             = [aws_key_pair.project1_key_pair]
+  ami                    = data.aws_ami.ubuntu.id // Fetching ami id using datasource
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.project1_key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.project1_sg.id]
+  subnet_id              = aws_subnet.Public_subnet_az1.id
+  tags = {
+    Name = "instance-az1"
+  }
+}
+
+# Create instance in az2
+resource "aws_instance" "pub_instance2" {
+  depends_on             = [aws_key_pair.project1_key_pair]
+  ami                    = data.aws_ami.ubuntu.id // Fetching ami id using datasource
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.project1_key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.project1_sg.id]
+  subnet_id              = aws_subnet.Public_subnet_az2.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "instance-az2"
+  }
 }
