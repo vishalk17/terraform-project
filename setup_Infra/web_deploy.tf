c798d1e2a0fe86829d912d8292937dec27c2574c
vishalk17 <vishalkapadi95@gmail.com>
2024-08-29 00:42:12 +0530

    deploy web1

---
+++ setup_Infra/web_deploy.tf
@@ -0,0 +1,12 @@
+resource "null_resource" "deploy_web1" {
+    depends_on = [ aws_instance.pub_instance1 ]
+    connection {
+        type        = "ssh"
+        host        = aws_instance.pub_instance1.public_ip
+        user        = "ubuntu"
+        private_key = file("${path.module}/keys/id_rsa")
+        }
+        provisioner "remote-exec" {
+            script = "web1_deploy.sh"
+        }
+}
