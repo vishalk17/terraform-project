2bea3332950c87f9d1859263c2fec490ee21d9fb
vishalk17 <vishalkapadi95@gmail.com>
2024-08-31 00:34:30 +0530

    remote_backend.tf: s3 and dynamodb must be active before setting up backend

--- setup_Infra/remote_backend.tf
+++ setup_Infra/remote_backend.tf
@@ -7,32 +7,3 @@ terraform {
     region         = "ap-south-1" // Terraform does not support using variables directly in the backend configuration.
   }
 }
-
-# Ref.: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "rb-s3" {
-  bucket = "project1-remote-backend-terraform"
-    region = var.aws_region
-  tags = {
-    Environment = "production"
-  }
-}
-
-# Ref.: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
-resource "aws_s3_bucket_versioning" "version-s3" {
-  depends_on = [aws_s3_bucket.rb-s3]
-  bucket     = aws_s3_bucket.rb-s3.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Ref.: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
-resource "aws_dynamodb_table" "name" {
-  name         = "project1-remote-backend-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
